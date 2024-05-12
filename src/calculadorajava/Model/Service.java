5949583dc246f7ed202f307ca1dbd3a59c8301d5
EduardoRibero <ribeirodomingues2020@gmail.com>
2024-05-12 16:15:54 -0300

    Class Service e metodos

--- src/calculadorajava/Model/Service.java
+++ src/calculadorajava/Model/Service.java
@@ -2,4 +2,19 @@ package calculadorajava.Model;
 
 public class Service {
 	
+	public double somar(double valor1,double valor2) {
+		return valor1 + valor2;
+	}
+	
+	public double subtrair(double valor1,double valor2) {
+		return valor1 - valor2;
+	}
+	
+	public double dividir(double valor1,double valor2) {
+		return valor1 / valor2;
+	}
+	
+	public double multiplicar(double valor1,double valor2) {
+		return valor1 * valor2;
+	}
 }
