64eb6687485ddf9ad6fca8a3ef026494d201791a
EduardoRibero <ribeirodomingues2020@gmail.com>
2024-05-13 10:13:32 -0300

    Conexão com o banco de dados realizada.

--- src/calculadorajava/Main.java
+++ src/calculadorajava/Main.java
@@ -1,10 +1,13 @@
 package calculadorajava;
 
+import calculadorajava.model.Repository;
 import calculadorajava.view.Tela;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException {
+		Repository jpa = new Repository();
+		jpa.conectar();
 		new Tela();
 
 	}
--- src/calculadorajava/model/Repository.java
+++ src/calculadorajava/model/Repository.java
@@ -1,5 +1,26 @@
 package calculadorajava.model;
 
-public class Repository {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class Repository {
+	
+	public void conectar () throws ClassNotFoundException {
+		String url = "jdbc:postgresql://localhost:5432/db_calculadorajava";
+        String user = "postgres";
+        String password = "admin";
+        
+        
+        try {
+            Connection connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Conexão bem-sucedida!");
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("Falha na conexão!");
+            e.printStackTrace();
+        }
+	}
+	
+	
 }
